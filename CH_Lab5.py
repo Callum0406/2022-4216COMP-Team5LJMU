9cae3735f55d17eeee0e209286b82811c876c06f
Charlie Hazlehurst <charliehazlehurst31@gmail.com>
2023-02-17 12:25:06 +0000

    edited CH_Lab5 file

---
+++ CH_Lab5.py
@@ -0,0 +1,16 @@
+#Lab 2 Section:
+#3.1 Names:
+names = ['Ben', 'Andrico', 'Rhys']
+msg1 = "Ben"
+msg2 = "Andrico"
+msg3 = "Rhys"
+print(names)
+greeting = "Hello"
+print(greeting, msg1)
+print(greeting, msg2)
+print(greeting, msg3)
+locations = ['USA', 'Italy', 'Barcelona', 'Maldives', 'Canada']
+own = "I would like to own"
+bike1 = "a Honda"
+own_bike1 = (own + bike1)
+print (own_bike1)
