40099bdcabfa9e7288c12785bca112f2efa2c55a
Charlie Hazlehurst <charliehazlehurst31@gmail.com>
2023-02-18 14:05:01 +0000

    Updated CH_Lab5.py

--- CH_Lab5.py
+++ CH_Lab5.py
@@ -1,16 +1,104 @@
+#Lab 1 Section:
+
+#1.1 python.org
+
+#1.2 Hello World Typos
+
+#1.3 Infinite Skills: 1. Tech Repair Website/App 2. Meal Prep/Planner App. 3. Budgeter App. 
+
 #Lab 2 Section:
-#3.1 Names:
-names = ['Ben', 'Andrico', 'Rhys']
-msg1 = "Ben"
-msg2 = "Andrico"
-msg3 = "Rhys"
-print(names)
-greeting = "Hello"
-print(greeting, msg1)
-print(greeting, msg2)
-print(greeting, msg3)
-locations = ['USA', 'Italy', 'Barcelona', 'Maldives', 'Canada']
-own = "I would like to own"
-bike1 = "a Honda"
-own_bike1 = (own + bike1)
-print (own_bike1)
+
+#2.1 Simple Message: 
+#2.2 Simple Messages:
+#message = "Hello, Python Crash Course reader (O'Reilley)"
+#new_message = "Hello, Python Crash Course reader (O'Reilley)"
+#print(message)
+#print(new_message)
+
+#2.3 Personal Messages:
+#p1_first_name = "Eric"
+#p1_last_name = "Jones"
+#print(p1_first_name)
+#print(p1_last_name)
+
+#2.4 Name Cases: 
+#p1_full_name = p1_first_name + p1_last_name
+#print(p1_full_name)
+#print(p1_full_name.upper())
+#print(p1_full_name.lower())
+#print(p1_full_name.title())
+
+#2.5 Famous Quote: 
+#fav_author = "Albert Einstein once said, "
+#fav_quote = '“A person who never made a mistake never tried anything new.”'
+#print(fav_author + fav_quote)
+
+#2.6 Famous Quote v2:
+#famous_person = "Walt Disney once said, "
+#famous_quote = '"The way to get started is to quit talking and begin doing."'
+#message = (famous_person + famous_quote)
+#print(message)
+
+#2.7 Stripping Names:
+#my_name = ' Charlie '
+#print(my_name.rstrip())
+#print(my_name.lstrip())
+#print(my_name.strip())
+
+#2.8 Number Eight
+#print(5+3)
+#print(11-3)
+#print(8*1)
+#print(8/1)
+
+#2.9 Favourite Number
+#fav_num = 21
+#message = (fav_num)
+#print(message)
+
+#2.10 Adding Comments
+#Say hello to everyone.
+#print("Hello, Python People!")
+
+#2.11 The Zen of Python
+#import this
+
+#Lab 3 Section:
+
+#3.1 Names/Lists:
+#names = ['Ben', 'Andrico', 'Rhys']
+#print(names[0])
+#print(names[1])
+#print(names[2])
+
+#3.2 Greetings:
+#names = ['Ben', 'Andrico', 'Rhys']
+#greeting = "Hello, "
+#print(greeting + names[0])
+#print(greeting + names[1])
+#print(greeting + names[2])
+#OR
+#msg1 = "Ben"
+#msg2 = "Andrico"
+#msg3 = "Rhys"
+#print(greeting, msg1)
+#print(greeting, msg2)
+#print(greeting, msg3)
+
+#3.3 Your Own List:
+#bike_list = ['Honda', 'Ducati', 'Surron']
+#fav_veh = "I would like to own a " 
+#print(fav_veh + (bike_list[0]))
+#print(fav_veh + (bike_list[1]))
+#print(fav_veh + (bike_list[2]))
+
+#3.4 Guest List:
+#guest_list = ['Andrico. ', 'Ben ', 'Rhys ']
+#message1 = "Dear, " + (guest_list[0]) + "I would like to respectfully invite you to my party tonight!"
+#message2 = "Dear, " + (guest_list[1]) + "I would like to respectfully invite you to my party tonight!"
+#message3 = "Dear, " + (guest_list[2]) + "I would like to respectfully invite you to my party tonight!"
+#print(message1)
+#print(message2)
+#print(message3)
+
+#3.5 Changing Guest List
