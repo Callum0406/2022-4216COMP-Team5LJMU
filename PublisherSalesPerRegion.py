0a91afb491a3ec1ca8fd0bddafcec1dabdf79d69
Louis Roberts <louis.roberts.wlc@gmail.com>
2023-03-17 14:24:10 +0000

    Finalised the code

--- PublisherSalesPerRegion.py
+++ PublisherSalesPerRegion.py
@@ -1,27 +1,54 @@
 import pandas as pd
-import matplotlib as matplot
+import matplotlib.pyplot as matplot
 
 #Read in the data from the CSV
 DATA = pd.read_csv("Tagged-Data-Final.csv")
 
 
-def publisherSales(region):
-    #Group the data based on the publisher column and sum that data up. Specifying the data found in the NA_Sales column.
-    publisherSales = DATA.groupby("Publisher").sum(numeric_only=True)[region].reset_index()
-    #Sort the data into descending order. Starting from the the best selling publishers. Limit the data to the top 5 publishers.
-    publisherSales = publisherSales.sort_values(region, ascending=False).head(5)
-    #print(publisherSales)
-    return publisherSales
+def top5publisherSalesNA(region):
 
 
-def graphSales(region):
-    salesData = publisherSales(region)
-    print(salesData)
+    #Check to see what region has been selected
+    if region == "NA_Sales":
+        #Group the data based on the publisher column and sum that data up. Specifying the data found in the NA_Sales column.
+        publisherData = DATA.groupby("Publisher").sum(numeric_only=True)[region].reset_index()
+        #Sort the data into descending order. Starting from the the best selling publishers. Limit the data to the top 5 publishers.
+        publisherData = publisherData.sort_values(region, ascending=False).head(5)
 
+        #Get the values for the chart
+        publisherNames = publisherData.Publisher
+        gameSales = publisherData.NA_Sales
+    
+        
+        #Set up the bar chart
+        matplot.bar(publisherNames, gameSales)
 
-graphSales("EU_Sales")
+        #Set graph labels and title.
+        matplot.xlabel("Publishers")
+        matplot.ylabel("Sales")
+        #matplot.title("Total Sales in North America")
 
+        #Show the bar chart
+        matplot.show()
+    
+    elif region == "EU_Sales":
 
+        publisherData = DATA.groupby("Publisher").sum(numeric_only=True)[region].reset_index()
+        #Sort the data into descending order. Starting from the the best selling publishers. Limit the data to the top 5 publishers.
+        publisherData = publisherData.sort_values(region, ascending=False).head(5)
 
-# test = publisherSales("EU_Sales")
-# print(test)
+        publisherNames = publisherData.Publisher
+        gameSales = publisherData.EU_Sales
+        
+        #Set up the bar chart
+        matplot.bar(publisherNames, gameSales)
+
+        #Set graph labels and title.
+        matplot.xlabel("Publishers")
+        matplot.ylabel("Sales")
+        #matplot.title("Total Sales in North America")
+
+        #Show the bar chart
+        matplot.show()
+
+top5publisherSalesNA("EU_Sales")
