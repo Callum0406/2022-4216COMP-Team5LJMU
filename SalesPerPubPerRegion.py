06708cac24decdb8c3ef1694f41c28e5c1c8b90d
Louis Roberts <louis.roberts.wlc@gmail.com>
2023-03-17 01:10:30 +0000

    Actually made something sane

--- SalesPerPubPerRegion.py
+++ SalesPerPubPerRegion.py
@@ -3,133 +3,16 @@ import pandas as pd
 #Read in the data from the CSV
 DATA = pd.read_csv("Tagged-Data-Final.csv")
 
-#Take the total NA sales of each publisher.
-#Publishers that are listed less than 10 times will be counted as "Other"
 
-#Data for JP has been left out due to being incomplete
-
-def publisherNASales():
-    
-    #3DO sales Total
-    _3DOData = DATA[(DATA["Publisher"] == "3DO")]
-    _3DONASales = _3DOData["NA_Sales"]
-    _3DOTotalNASales = round(sum(_3DONASales), 2)
-
-    #505 Games Total
-    _505GamesData = DATA[(DATA["Publisher"] == "505 Games")]
-    _505GamesNASales = _505GamesData["NA_Sales"]
-    _505GamesTotalNASales = round(sum(_505GamesNASales), 2)
-
-    #Acclaim Total
-    AcclaimData = DATA[(DATA["Publisher"] == "Acclaim Entertainment")]
-    AcclaimNASales = AcclaimData["NA_Sales"]
-    AcclaimTotalNASales = round(sum(AcclaimNASales), 2)
-
-    #Activision Total
-    ActivisionBaseData = DATA[(DATA["Publisher"] == "Activision")]
-    ActivisionValueData = DATA[(DATA["Publisher"] == "Activision Value")]
-    ActivisionBlizzardData = DATA[(DATA["Publisher"] == "Activision Blizzard")]
-    ActivisionBaseNASales = ActivisionBaseData["NA_Sales"]
-    ActivisionValueNASales = ActivisionValueData["NA_Sales"]
-    ActivisionBlizzardNASales = ActivisionBlizzardData["NA_Sales"]
-    ActivisionTotalNASales = round(sum(ActivisionBaseNASales) + sum(ActivisionBlizzardNASales) + sum(ActivisionValueNASales), 2)
-
-    #Atari Total
-    AtariData = DATA[(DATA["Publisher"] == "Atari")]
-    AtariNASales = AtariData["NA_Sales"]
-    AtariTotalNASales = round(sum(AtariNASales), 2)
-
-    #Bethesda Total
-    BethesdaData = DATA[(DATA["Publisher"] == "Bethesda Softworks")]
-    BethesdaNASales = BethesdaData["NA_Sales"]
-    BethesdaTotalNASales = round(sum(BethesdaNASales), 2)
-
-    #Capcom Total
-    CapcomData = DATA[(DATA["Publisher"] == "Capcom")]
-    CapcomNASales = CapcomData["NA_Sales"]
-    CapcomTotalNASales = round(sum(CapcomNASales), 2)
-
-    #Codemasters / Codemasters Online
-    CodemastersData = DATA[(DATA["Publisher"] == "Codemasters")]
-    CodemastersOnlineData = DATA[(DATA["Publisher"] == "Codemasters Online")]
-    CodemastersNASales = CodemastersData["NA_Sales"]
-    CodeMastersOnlineNASales = CodemastersOnlineData["NA_Sales"]
-    CodemastersTotalNASales = round(sum(CodemastersNASales) + sum(CodeMastersOnlineNASales),2)
-
-    #D3Publisher
-    D3PublisherData = DATA[(DATA["Publisher"] == "D3Publisher")]
-    D3PublisherNASales = D3PublisherData["NA_Sales"]
-    D3PublisherTotalNASales = round(sum(D3PublisherNASales))
-
-    #Deep Silver
-    DeepSilverData = DATA[(DATA["Publisher"] == "Deep Silver")]
-    DeepSilverNASales = DeepSilverData["NA_Sales"]
-    DeepSilverTotalNASales = round(sum(DeepSilverNASales))
-
-    #Disney
-    DisneyData = DATA[(DATA["Publisher"] == "Disney Interactive Studios")]
-    DisneyNASales = DisneyData["NA_Sales"]
-    DisneyTotalNASales = round(sum(DisneyNASales))
-    
-    #EA Games
-    EAGamesData = DATA[(DATA["Publisher"] == "EAGames")]
-    ElectronicArtsData = DATA[(DATA["Publisher"] == "Electronic Arts")]
-    EAGamesNASales = EAGamesData["NA_Sales"]
-    ElectronicArtsNASales = ElectronicArtsData["NA_Sales"]
-    EAGamesTotalNASales = round(sum(EAGamesNASales) + sum(ElectronicArtsNASales),2)
-
-    #Eidos
-    EidosData = DATA[(DATA["Publisher"] == "Eidos Interactive")]
-    EidosNASales = EidosData["NA_Sales"]
-    EidosTotalNASales = round(sum(EidosNASales))
-
-    #Empire Interactive
-    EmpireIntData = DATA[(DATA["Publisher"] == "Empire Interactive")]
-    EmpireIntNASales = EmpireIntData["NA_Sales"]
-    EmpireIntTotalNASales = round(sum(EmpireIntNASales))
-
-    #Global Star
-    GlobalStarData = DATA[(DATA["Publisher"] == "Global Star")]
-    GlobalStarNASales = GlobalStarData["NA_Sales"]
-    GlobalStarTotalNASales = round(sum(GlobalStarNASales))
-
-    #Ignition Entertainment
-    IgnitionEntData = DATA[(DATA["Publisher"] == "Ignition Entertainment")]
-    IgnitionEntNASales = IgnitionEntData["NA_Sales"]
-    IgnitionEntTotalNASales = round(sum(IgnitionEntNASales))
-
-    #Infogrames
-    InfogramesData = DATA[(DATA["Publisher"] == "Infogrames")]
-    InfogramesNASales = InfogramesData["NA_Sales"]
-    InfogramesTotalNASales = round(sum(InfogramesNASales))
-
-    #Just for testing. Template print("Pub:  " + str(PubTotalNASales) + " Million")
-    print("Total Sales")
-    print("3DO:  " + str(_3DOTotalNASales) + " Million")
-    print("505 Games:  " + str(_505GamesTotalNASales) + " Million")
-    print("Acclaim Entertainment:  " + str(AcclaimTotalNASales) + " Million")
-    print("Activision:  " + str(ActivisionTotalNASales) + " Million")
-    print("Atari:  " + str(AtariTotalNASales) + " Million")
-    print("Bethesda:  " + str(BethesdaTotalNASales) + " Million")
-    print("Capcom:  " + str(CapcomTotalNASales) + " Million")
-    print("Codemasterss:  " + str(CodemastersTotalNASales) + " Million")
-    print("D3Publisher:  " + str(D3PublisherTotalNASales) + " Million")
-    print("DeepSilver:  " + str(DeepSilverTotalNASales) + " Million")
-    print("Disney:  " + str(DisneyTotalNASales) + " Million")
-    print("EA Games:  " + str(EAGamesTotalNASales) + " Million")
-    print("Eidos Interactive:  " + str(EidosTotalNASales) + " Million")
-    print("Empire Interactive:  " + str(EmpireIntTotalNASales) + " Million")
-    print("Global Star:  " + str(GlobalStarTotalNASales) + " Million")
-    print("Ignition Entertainment:  " + str(IgnitionEntTotalNASales) + " Million")
-    print("Infogrames:  " + str(InfogramesTotalNASales) + " Million")
+def publisherSales(region):
+    #Group the data based on the publisher column and sum that data up. Specifying the data found in the NA_Sales column.
+    publisherSales = DATA.groupby("Publisher").sum(numeric_only=True)[region].reset_index()
+    #Sort the data into descending order. Starting from the the best selling publishers. Limit the data to the top 5 publishers.
+    publisherSales = publisherSales.sort_values(region, ascending=False).head(5)
+    print(publisherSales)
 
 
-publisherNASales()
 
-
-#Left out due to incomplete data: 
-#daedalic, Devolver Digital, DSI games, Dusenberry Martin Racing, Encore, 
-#Focus Home Interactive, Funcom, FuRyu, Gathering of Developers, Gearbox Software, 
-#Genki, Global A Entertainment, GOA, Graffiti, Graphsim Entertainment, Gust, 
-#Headup Games, Hello Games, HMH Interactive, Iceberg Interactive, id Software
-#
+publisherSales("NA_Sales")
+print("----")
+publisherSales("EU_Sales")
