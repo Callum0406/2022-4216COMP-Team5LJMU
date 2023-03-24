17d56a28d383adfc77748713c1a11ea9f5a21ae8
Joe Moran <csmjmora@ljmu.ac.uk>
2023-03-24 13:40:11 +0000

    Trying out pie chart for genre & region sales.

--- test.py
+++ test.py
@@ -1,11 +1,31 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+## Sales by Genre Pie Chart
 
 data = pd.read_csv("Tagged-Data-Final.csv")
 
-C = data["Genre"]
+salesRegion = "NA_Sales"
+#Data grouped up by genre and 
+compiled_data = data.groupby("Genre").sum(numeric_only=True)[salesRegion].reset_index()
+compiled_data = compiled_data.sort_values(salesRegion, ascending=False).head(5)
 
-print(C)
+genre_label = compiled_data.Genre
+region_name = compiled_data.NA_Sales
 
+
+
+plt.pie(region_name, labels=genre_label, autopct='%1.1f%%')
+plt.title('NA Sales by Genre')
+plt.axis('equal')
+plt.show()
+
+
+#genreData = data["Genre"]
+#salesData = data["NA_Sales"]
+
+#print(salesData)
 
 #x = pd.DataFrame([293, 3949, 4994, 290])
 #print(x)
