710396e78385f8cec9ddab9abd43c30d747c5f09
callum0406 <callumdoran91@gmail.com>
2023-03-24 13:40:47 +0000

    Ensuring print functions work

---
+++ dataCallum.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib as matplot
+
+Data = pd.read_csv("Tagged-Data-Final.csv")
+#read in file data
+
+print("This is video game based on critic score")
+
+print("Do you want to see critic scores from lowest to highest or highest to lowest")
+ 
