718fbcdcee8d778d6e7eab9b5997e9971fe3d648
callum0406 <callumdoran91@gmail.com>
2023-03-30 16:08:43 +0100

    Added bar chart to code

--- dataCallum.py
+++ dataCallum.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import matplotlib as matplot
-
-Data = pd.read_csv("Tagged-Data-Final.csv")
+#import pandas and matplotlib for data reading and bar chart creation
+data = pd.read_csv("Tagged-Data-Final.csv")
 #read in file data
 
-print("This is video game based on critic score")
 
-print("Do you want to see critic scores from lowest to highest or highest to lowest")
- 
+print("This is video games based on critic score")
+
+x = data['Name']
+y = data['Critic_Score']
+
+plt.bar(x, y)
+
+plt.xlabel('Game name')
+plt.ylabel('Y-axis label')
+plt.title('Games by user critic score')
+
+plt.show
