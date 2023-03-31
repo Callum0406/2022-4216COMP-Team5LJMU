ebc70f53bebee9c09ed4c68c077def365eacb149
callum0406 <callumdoran91@gmail.com>
2023-03-31 11:33:30 +0100

    Remove faulty code

--- dataCallum.py
+++ dataCallum.py
@@ -1,19 +1,8 @@
 import pandas as pd
-import matplotlib as matplot
+import matplotlib as mt
 #import pandas and matplotlib for data reading and bar chart creation
 data = pd.read_csv("Tagged-Data-Final.csv")
 #read in file data
 
-
 print("This is video games based on critic score")
 
-x = data['Name']
-y = data['Critic_Score']
-
-plt.bar(x, y)
-
-plt.xlabel('Game name')
-plt.ylabel('Y-axis label')
-plt.title('Games by user critic score')
-
-plt.show
