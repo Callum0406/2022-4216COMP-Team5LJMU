dfecfce7b725aa19bb1465a0ebf216f88705a0bd
callum0406 <callumdoran91@gmail.com>
2023-03-31 12:42:48 +0100

    Removed test code added bar chart

--- dataCallum.py
+++ dataCallum.py
@@ -1,8 +1,22 @@
 import pandas as pd
-import matplotlib as mt
-#import pandas and matplotlib for data reading and bar chart creation
+import matplotlib.pyplot as matplot
+#import necessary packages
+
 data = pd.read_csv("Tagged-Data-Final.csv")
-#read in file data
+#import data from excel file
 
-print("This is video games based on critic score")
+extracted_data = data.sort_values("Critic_Score", ascending=False).head(10)
+#shows data descending 
+name = extracted_data.Name
+criticsc = extracted_data.Critic_Score
 
+matplot.bar(name, criticsc)
+matplot.title("Top Ten Games By Critic Score")
+
+matplot.xlabel("Name")
+matplot.ylabel("Critic Score")
+#axis labels
+matplot.show
+#produces bar chart
+    
+   
