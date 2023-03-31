5407dfd8d9fae1a2cc2fd498ee0cee29f2150fae
Charlie Hazlehurst <charliehazlehurst31@gmail.com>
2023-03-31 11:50:03 +0100

    Updated SalesbyDeveloper.py

--- SalesbyDeveloper.py
+++ SalesbyDeveloper.py
@@ -55,24 +55,24 @@ def top_5_developer_sales(globals):
 
 top_5_developer_sales("EU_Sales")
 
-if globals == "NA_Sales":
-
-    dev_data = DATA.groupby("Developer").sum(numeric_only=True)[globals].reset_index()
+#if globals == "NA_Sales":
+        
+        #developer_data = DATA.groupby("Developer").sum(numeric_only=True)[globals].reset_index()
 
         #Sort the data into descending order. Starting from the the best selling publishers. Limit the data to the top 5 publishers.
-dev_data = dev_data.sort_values(globals, ascending=False).head(5)
-dev_names = dev_data.Developer
-game_sales = dev_data.NA_Sales
+        #developer_data = developer_data.sort_values(globals, ascending=False).head(5)
+        #developer_names = developer_data.Developer
+        #game_sales = developer_data.NA_Sales
         
         #Set up the bar chart
-matplot.bar(dev_names, game_sales)
+        #matplot.bar(developer_names, game_sales)
 
         #Set graph labels and title.
-matplot.title("Best Selling Developer in North America")
-matplot.xlabel("Developer")
-matplot.ylabel("Sales")
+        #matplot.title("Best Selling Developer in North America")
+        #matplot.xlabel("Developer")
+        #matplot.ylabel("Sales")
         
         #Show the bar chart
-matplot.show()
+        #matplot.show()
 
-top_5_developer_sales("NA_Sales")
+#top_5_developer_sales("NA_Sales")
