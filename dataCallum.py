5ddf6e8ccdcad94c573aa431929177bb6684d5f5
callum0406 <callumdoran91@gmail.com>
2023-04-11 15:43:14 +0100

    added option into my code to choose between different graphs

--- dataCallum.py
+++ dataCallum.py
@@ -1,22 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as matplot
 #import necessary packages
-def dataCallum():
-    data = pd.read_csv("Tagged-Data-Final.csv")
-    #import data from excel file
 
-    extracted_data = data.sort_values("Critic_Score", ascending=False).head(10)
-    #shows data descending 
-    name = extracted_data.Name
-    criticsc = extracted_data.Critic_Score
+data = pd.read_csv("Tagged-Data-Final.csv")
+#import data from excel file
 
-    matplot.bar(name, criticsc)
-    matplot.title("Top Ten Games By Critic Score")
+def graph(option):
+    if option == 1:
+        extracted_data = data.sort_values("Critic_Score", ascending=False).head(10)
+            #shows data descending 
+        name = extracted_data.Name
+        criticsc = extracted_data.Critic_Score
 
-    matplot.xlabel("Name")
-    matplot.ylabel("Critic Score")
-    #axis labels
-    matplot.show()
-    #produces bar chart
+        matplot.bar(name, criticsc)
+        matplot.title("Top Ten Games By Critic Score")
+
+        matplot.xlabel("Name")
+        matplot.ylabel("Critic Score")
+        #axis labels
+        matplot.show()
+        #produces bar chart
+        quit()
+    elif option == "2":
+        extracted_data = data.sort_values("Critic_Score", ascending=True).head(10)
+            #shows data descending 
+        name = extracted_data.Name
+        criticsc = extracted_data.Critic_Score
+
+        matplot.bar(name, criticsc)
+        matplot.title("Ten Lowest Games By Critic Score")
+
+        matplot.xlabel("Name")
+        matplot.ylabel("Critic Score")
+        #axis labels
+        matplot.show()
+        #produces bar chart
+        quit()
     
    
