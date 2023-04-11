3f623272b5b1abf34bf008067105db40de0f57ca
Joe Moran <csmjmora@ljmu.ac.uk>
2023-04-11 14:47:56 +0100

    f

--- GenrePieChartData.py
+++ GenrePieChartData.py
@@ -4,17 +4,19 @@ import numpy as np
 
 ## Sales by Genre Pie Chart
 
-data = pd.read_csv("Tagged-Data-Final.csv")
+def genreToSales():
 
-salesRegion = "NA_Sales"
-#Data grouped up by genre and 
-compiled_data = data.groupby("Genre").sum(numeric_only=True)[salesRegion].reset_index()
-compiled_data = compiled_data.sort_values(salesRegion, ascending=False).head(5)
+    data = pd.read_csv("Tagged-Data-Final.csv")
 
-genre_label = compiled_data.Genre
-region_name = compiled_data.NA_Sales
+    salesRegion = "NA_Sales"
+    #Data grouped up by genre and 
+    compiled_data = data.groupby("Genre").sum(numeric_only=True)[salesRegion].reset_index()
+    compiled_data = compiled_data.sort_values(salesRegion, ascending=False).head(5)
 
-plt.pie(region_name, labels=genre_label, autopct='%1.1f%%')
-plt.title('Top 5 NA Sales by Genre')
-plt.axis('equal')
-plt.show()
+    genre_label = compiled_data.Genre
+    region_name = compiled_data.NA_Sales
+
+    plt.pie(region_name, labels=genre_label, autopct='%1.1f%%')
+    plt.title('Top 5 NA Sales by Genre')
+    plt.axis('equal')
+    plt.show()
