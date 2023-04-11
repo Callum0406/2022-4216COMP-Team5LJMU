d32ac21c8fad5dd3ee03337457d688303971b016
Joe Moran <csmjmora@ljmu.ac.uk>
2023-04-11 14:35:58 +0100

    Testing menu

--- menu.py
+++ menu.py
@@ -1,10 +1,12 @@
+import GenrePieChartData as PieData
+
 def main_menu():
     print("--MENU--")
     print("1. Option 1")
     print("2. Option 2")
     print("3. Option 3")
     print("4. Option 4")
-    print("5. Option 5")
+    print("5. Display sales by genre")
     print("6. Exit")
 
 loop=True
@@ -27,6 +29,7 @@ while loop:
         loop=False
     elif choice==5:
         print("Option 5 Selected")
+        print(PieData)
         loop=False
     elif choice==6:
         print ("Exiting...")
