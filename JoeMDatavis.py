930cba9a90afd6e49a3d204d40bb022849eaa2e6
JoeMoran1 <123165599+JoeMoran1@users.noreply.github.com>
2023-04-20 01:09:16 +0100

    Update JoeMDatavis.py
    Added comments

--- JoeMDatavis.py
+++ JoeMDatavis.py
@@ -5,17 +5,17 @@ import numpy as np
 ## Sales by Genre Pie Chart
 
 def genreToSales():
-
+    #Creating a data variable to store the read data.
     data = pd.read_csv("Tagged-Data-Final.csv")
 
     salesRegion = "NA_Sales"
-    #Data grouped up by genre and 
+    #Data grouped up by genre, sorted and limited to 5.
     compiled_data = data.groupby("Genre").sum(numeric_only=True)[salesRegion].reset_index()
     compiled_data = compiled_data.sort_values(salesRegion, ascending=False).head(5)
 
     genre_label = compiled_data.Genre
     region_name = compiled_data.NA_Sales
-
+    #Creating a pie chart.
     plt.pie(region_name, labels=genre_label, autopct='%1.1f%%')
     plt.title('Top 5 NA Sales by Genre')
     plt.axis('equal')
